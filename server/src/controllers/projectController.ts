b50f4653a37a3375a3b72d138d8ac1fb1ae7fe4c
Liangce Li <liliangce316@gmail.com>
2025-04-21 17:16:21 +0930

    fix

--- server/src/controllers/projectController.ts
+++ server/src/controllers/projectController.ts
@@ -1,38 +1,40 @@
 import { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient;
+const prisma = new PrismaClient();
 
 export const getProjects = async (
-    req: Request,
-    res: Response,
+  req: Request,
+  res: Response
 ): Promise<void> => {
-    try {
-        const projects = await prisma.project.findMany();
-        res.json(projects);
-    } catch (error:any) {
-        res.status(500).json({message: `Error retrieving projects ${error.message}`})
-    }
-}
+  try {
+    const projects = await prisma.project.findMany();
+    res.json(projects);
+  } catch (error: any) {
+    res
+      .status(500)
+      .json({ message: `Error retrieving projects: ${error.message}` });
+  }
+};
 
 export const createProject = async (
-    req:Request,
-    res:Response,
+  req: Request,
+  res: Response
 ): Promise<void> => {
-    const { name, description, startDate, endDate} = req.body;
-    try {
-        const newProject = await prisma.project.create({
-            data: {
-                name,
-                description,
-                startDate,
-                endDate,
-            },
-        });
-        res.status(201).json(newProject)
-    } catch (error:any) {
-        res.status(500).json({message: `Error creating projects: ${error.message}`})
-    }
-}
-
-// SELECT setval(pg_get_serial_sequence('"Project"', 'id'),coalesce(max(id)+1,1),false) FROM "Project";
+  const { name, description, startDate, endDate } = req.body;
+  try {
+    const newProject = await prisma.project.create({
+      data: {
+        name,
+        description,
+        startDate,
+        endDate,
+      },
+    });
+    res.status(201).json(newProject);
+  } catch (error: any) {
+    res
+      .status(500)
+      .json({ message: `Error creating a project: ${error.message}` });
+  }
+};
