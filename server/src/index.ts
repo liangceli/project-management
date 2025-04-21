51ef954c313c9187ad3cb519026c19dc054d64c1
Liangce Li <liliangce316@gmail.com>
2025-04-21 20:05:22 +0930

    fix

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,10 +4,9 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
-
 /* ROUTE IMPORTS */
 import projectRoutes from "./routes/projectRoutes";
-import taskRoutes from "./routes/taskRoute";
+import taskRoutes from "./routes/taskRoutes";
 import searchRoutes from "./routes/searchRoutes";
 import userRoutes from "./routes/userRoutes";
 import teamRoutes from "./routes/teamRoutes";
@@ -24,17 +23,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 /* ROUTES */
-app.get("/", (req,res) => {
-    res.send("This is home route");
+app.get("/", (req, res) => {
+  res.send("This is home route");
 });
+
 app.use("/projects", projectRoutes);
 app.use("/tasks", taskRoutes);
 app.use("/search", searchRoutes);
 app.use("/users", userRoutes);
 app.use("/teams", teamRoutes);
 
-/**SERVER */
+/* SERVER */
 const port = Number(process.env.PORT) || 3000;
 app.listen(port, "0.0.0.0", () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on part ${port}`);
 });
